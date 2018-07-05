ec7a8859a36f0d35a370bdbc0c3693a9820865b0
RSicurella <riccardo.sicurella@adcubum.com>
2018-07-05 15:08:22 +0200

    test

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,11 +9,26 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        if(Math.random() > 0.1) {
 
-            return 3000 * (int)Math.random();
+
+        try {
+            int i = request.getAsJsonObject().getAsJsonObject("round").getAsInt();
+            if (i<15) return 0;
+
+            if (Math.random() > 0.1) {
+
+                return 3000 * (int) Math.random();
+            }
+            return 0;
+        } catch (Exception e) {
+
+            if (Math.random() > 0.1) {
+
+                return 3000 * (int) Math.random();
+            }
+            return 0;
         }
-        return 0;
+
     }
 
     public static void showdown(JsonElement game) {
