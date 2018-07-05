6db0ecf52176607fd27247c7cc81eec6e8212f84
Alexander Christ <alexander.christ@adcubum.com>
2018-07-05 13:58:48 +0200

    Test

---
+++ src/main/java/org/leanpoker/player/MainTest.java
@@ -0,0 +1,10 @@
+package org.leanpoker.player;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+public class MainTest {
+    public void test() {
+
+    }
+}
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,8 +9,11 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        if(Math.random() > 0.8) {
 
-        return 3000 * (int)Math.random();
+            return 3000 * (int)Math.random();
+        }
+        return 0;
     }
 
     public static void showdown(JsonElement game) {
